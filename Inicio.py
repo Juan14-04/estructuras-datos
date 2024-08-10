d662e5a61a0a392a5a373a9ef25bf790b072295d
juandallow1 <juandallow1@gmail.com>
2024-08-10 17:22:13 +0000

    agrago este nuevo paso

--- Inicio.py
+++ Inicio.py
@@ -1,1 +1,8 @@
-edad = 22 
+edad = 22
+edad = int(input("Introduce tu edad: "))
+
+if edad < 18:
+  print("Lo siento, no puedes entrar. Debes ser mayor de 18 años.")
+else:
+  print("Bienvenido!")
+
