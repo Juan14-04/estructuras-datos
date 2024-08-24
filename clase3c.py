8ed28b93b7315626da041ce20c47c4a5e641f60a
Juan14-04 <juandallow1@gmail.com>
2024-08-24 19:20:34 +0000

    clas a y d

---
+++ clase3c.py
@@ -0,0 +1,3 @@
+numeros=[5,9,5,6,8,6,1,5,6,9]
+promedio= sum(numeros)/len(numeros)
+print("el promedio de los numeros seleccionado son: ", promedio)
---
+++ clase3d.py
@@ -0,0 +1,4 @@
+numeros = int(input("ingrese el numero factorial: "))
+for x in range(1, numeros):
+   
+    print("los valores son: ")
