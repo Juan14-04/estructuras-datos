2bc4e7002225f1854a63cc8a07a047a292e5a299
Juan14-04 <juandallow1@gmail.com>
2024-08-24 17:53:17 +0000

    clase 3 ejercicios

---
+++ clase3a.py
@@ -0,0 +1,4 @@
+numeros= []
+for i in range(2, 101, 2):
+    numeros.append(i)
+print(numeros)
---
+++ clase3b.py
@@ -0,0 +1,6 @@
+# formar un triangulo con asteriscos 
+asteriscos= 5
+for i in range(1, asteriscos+1):
+    
+    print(i * "*")
+    
